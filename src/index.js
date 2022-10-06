cdc5e352f3a47454c5236db611ba6bcbca4f0a5c
Cyril <cj.welschen@gmail.com>
2022-10-06 21:54:19 +0200

    step 8: write to firestore DB!

--- src/index.js
+++ src/index.js
@@ -69,7 +69,8 @@ function initFirebaseAuth() {
 // Returns the signed-in user's profile Pic URL.
 function getProfilePicUrl() {
     // TODO 4: Return the user's profile pic URL.
-    return getAuth().currentUser.photoURL || '/images/profile_placeholder.png';
+    // return getAuth().currentUser.photoURL || '/images/profile_placeholder.png';
+    return '/images/profile_placeholder.png';
 }
 
 // Returns the signed-in user's display name.
@@ -87,6 +88,17 @@ function isUserSignedIn() {
 // Saves a new message on the Cloud Firestore.
 async function saveMessage(messageText) {
     // TODO 7: Push a new message to Cloud Firestore.
+    // Add a new message entry to the Firebase database.
+    try {
+        await addDoc(collection(getFirestore(), 'messages'), {
+            name: getUserName(),
+            text: messageText,
+            profilePicUrl: getProfilePicUrl(),
+            timestamp: serverTimestamp()
+        });
+    } catch (error) {
+        console.error('Error writing new message to Firebase Database', error);
+    }
 }
 
 // Loads chat messages history and listens for upcoming ones.
